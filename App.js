d6ad112245e6ccb18123eb72dac673f359e5da74
robin-hassan <robiul.hassan12102@gmail.com>
2020-06-19 16:07:58 +0800

    init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScanScreen from './src/screen/ScanScreen';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <ScanScreen />
+    </>
+  );
+};
+
+export default App;
---
+++ src/screen/ScanScreen.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Linking, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+
+class ScanScreen extends Component {
+    onSuccess = e => {
+        Linking.openURL(e.data).catch(err =>
+            console.error('An error occured', err)
+        );
+    };
+
+    render() {
+        return (
+            <QRCodeScanner
+                onRead={this.onSuccess}
+                flashMode={RNCamera.Constants.FlashMode.torch}
+                topContent={
+                    <Text style={styles.centerText}>
+                        Go to{' '}
+                            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text>
+                    </Text>
+                }
+                bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                }
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    centerText: {
+        flex: 1,
+        fontSize: 18,
+        padding: 32,
+        color: '#777'
+    },
+    textBold: {
+        fontWeight: '500',
+        color: '#000'
+    },
+    buttonText: {
+        fontSize: 21,
+        color: 'rgb(0,122,255)'
+    },
+    buttonTouchable: {
+        padding: 16
+    }
+});
+
+export default ScanScreen;
